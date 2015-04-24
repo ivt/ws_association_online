7fcf4b075e7b33e6dd624768cc3438e4eafc1462
Peter Serwylo <peter@ivt.com.au>
2015-04-24 16:38:47 +1000

    Provide a wrapper around core_user_create_users webservice.
    The wrapper is local_ws_association_online_create_users, and it is
    exactly the same (delegates to the original function) except that it
    allows users with the same emails as existing users to be created.
    
    This is inline with the feature provided by the Moodle batch importer,
    which allows people with the same email to be imported if a checkbox
    is selected.

--- version.php
+++ version.php
@@ -1,6 +1,6 @@
 <?php
 
-$plugin->version  = 2014102801;
+$plugin->version  = 2015041701;
 $plugin->requires = 2010112400;  // Requires this Moodle version - at least 2.0
 $plugin->cron     = 0;
 $plugin->release = '1.0 (Build: 2014071701)';
