60a39e05b2966536e491b051ad8c909080997d94
Tristan McLeay <tmcleay@advsol.com>
2019-05-23 14:39:58 +1000

    moodle 3+ compatibility
    add ->component property, bump versions.

--- version.php
+++ version.php
@@ -1,7 +1,8 @@
 <?php
 
-$plugin->version  = 2015041701;
+$plugin->component = "local_ws_association_online";
+$plugin->version  = 2019052301;
 $plugin->requires = 2010112400;  // Requires this Moodle version - at least 2.0
 $plugin->cron     = 0;
-$plugin->release = '1.0 (Build: 2014071701)';
+$plugin->release = '1.0.1 (Build: 2019052301)';
 $plugin->maturity = MATURITY_STABLE;
